4dd3c42cd1b9937cb3004e42e6354cb30e2943b0
Maheesha99 <mahishalunuwila@gmail.com>
2020-09-19 14:16:02 +0530

    remove cred from conf file

--- Utils/conf.js
+++ Utils/conf.js
@@ -1,8 +1,8 @@
 module.exports = {
 
   //browser stack common keys
-  user: 'maheesha1',
-  key: 'zD66vXbYCdopAyUpAZYc',
+  user: '',
+  key: '',
   build: '1.14.77-test',
   project: 'TravelText',
   appium_version: '1.15.0',
@@ -15,40 +15,10 @@ module.exports = {
   app_browserstack_android:'traveltext',
 
   //browserstack_ios_Default
-  deviceName_browserstack_ios:'iPhone 8 Plus', //'iPhone 11 Pro Max',//'iPhone 8 Plus',
-  platformVersion_browserstack_ios: '11',//'13',//'11',
-  sessionName_browserstack_ios:'iPhone 8 Plus applitools 2',
-  app_browserstack_ios:'traveltextiOS',
-
-  //browserstack_android_Device_1
-  deviceName_browserstack_android_D1:'Samsung Galaxy S20' ,//'Samsung Galaxy Tab S6',//'Samsung Galaxy A10','Samsung Galaxy S20',
-  platformVersion_browserstack_android_D1: '10',//'9',//'10',
-  sessionName_browserstack_android_D1:'Samsung Galaxy S20',
-  app_browserstack_android_D1:'traveltext',
-
-  //browserstack_android_Device_2
-  deviceName_browserstack_android_D2:'Samsung Galaxy A10' ,//'Samsung Galaxy Tab S6',//'Samsung Galaxy A10','Samsung Galaxy S20',
-  platformVersion_browserstack_android_D2: '9',//'9',//'10',
-  sessionName_browserstack_android_D2:'Samsung Galaxy A10',
-  app_browserstack_android_D2:'traveltext',
-
-  //browserstack_android_Device_3
-  deviceName_browserstack_android_D3:'Samsung Galaxy Tab S6' ,//'Samsung Galaxy Tab S6',//'Samsung Galaxy A10','Samsung Galaxy S20',
-  platformVersion_browserstack_android_D3: '9',//'9',//'10',
-  sessionName_browserstack_android_D3:'Samsung Galaxy Tab S6',
-  app_browserstack_android_D3:'traveltext',
-
-  //browserstack_ios_Device_1
-  deviceName_browserstack_ios_D1:'iPhone 8 Plus', //'iPhone 11 Pro Max',//'iPhone 8 Plus',
-  platformVersion_browserstack_ios_D1: '11',//'13',//'11',
-  sessionName_browserstack_ios_D1:'iPhone 8 Plus',
-  app_browserstack_ios_D1:'traveltextiOS',
-
-  //browserstack_ios_Device_2
-  deviceName_browserstack_ios_D2:'iPhone 11 Pro Max', //'iPhone 11 Pro Max',//'iPhone 8 Plus',
-  platformVersion_browserstack_ios_D2: '13',//'13',//'11',
-  sessionName_browserstack_ios_D2:'iPhone 11 Pro Max',
-  app_browserstack_ios_D2:'traveltextiOS',
+  deviceName_browserstack_ios:'iPad Mini 3', //'iPhone 11 Pro Max',//'iPhone 8 Plus',
+  platformVersion_browserstack_ios: '8',//'13',//'11',
+  sessionName_browserstack_ios:'iPad Mini 3 applitools 2',
+  app_browserstack_ios:'',
 
   //local_android
   deviceName_local_android: 'd5ccda7f',
@@ -57,7 +27,7 @@ module.exports = {
   //local_ios
   deviceName_local_ios: '99XT-IPHONE7',
   platformVersion_local_ios: '13.3',
-  udid_local_ios: 'd4458d89db84426aa4c350f3157d0c8ee3d889ab',
-  xcodeOrgId_local_ios: 'LTN2588C6H',
+  udid_local_ios: '',
+  xcodeOrgId_local_ios: '',
 
 }
